d3ca839cb4274c6ab1738c73160197ca627cf67b
x18127436 <x18127436@student.ncirl.ie>
2020-08-15 16:19:50 -0700

    First commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: "_openflights", domain: "localhost"
+#Rails.application.config.session_store :cookie_store, key: "_openflights", domain: "localhost"
+Rails.application.config.session_store :cookie_store, key: '_station_session'
